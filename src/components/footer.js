fdd3fbe10841e95ac80a0c04506818a0b6a9cc00
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2024-03-05 10:35:57 +0100

    responsivitet

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,36 @@
-import React from 'react'
-import { ContactButton } from './contactButton'
-import '../css/footer.css'
-
+import React from 'react';
+import '../css/footer.css';
+import { useModal } from './modalContext';
+ // Importera useModal-hook
+import '../css/button.css';
 
 export const Footer = () => {
+  const { openModal } = useModal();
+  const handleClick = () => {
+    window.open('https://calendly.com/separationscoach', '_blank');
+  }; 
   return (
     <footer className="footerGrid">
-      <div className="contact">
-        <div className="mail">
-           <ContactButton className="OverrideButton" />
-          
-          <div className="social">
-          <a href="https://www.instagram.com/ellatech.se" target="_blank" rel="noopener noreferrer" aria-label="Follow us on Instagram">
-            <i className="fab fa-instagram">popido</i>
-          </a>
-        </div>
-          <p className="copyRight">© EllaTech 2023</p>
-        </div>
-       
+      <div className="contactFooter">
+        {/* Kontakt-knappen som öppnar modalen */}
+        <button onClick={openModal} className="contactButtonFooter" aria-label="Kontakta mig">Kontakta mig</button>
+      </div>
+      <div className="bookingFooter">
+    <button
+      className="BookingButtonFooter"
+      type="button"
+      onClick={handleClick}
+      aria-label='Boka tjänst'>
+      Boka tid
+    </button>
+      </div>
+
+      <div className="social">
+        {/* Länk till Instagram */}
+        <a href="https://www.instagram.com/ellatech.se" target="_blank" rel="noopener noreferrer" aria-label="Follow me on Instagram">
+          <i className="instagram"> <p>Separationscoach </p></i>
+        </a>
       </div>
     </footer>
-  )
+  );
 }
